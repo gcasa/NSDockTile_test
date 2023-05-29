13df38cadc4bf41fc24e36b1b80a183682a0036d
Gregory John Casamento <greg.casamento@gmail.com>
2023-05-29 18:38:23 -0400

    Initial Commit

---
+++ NSDockIcon_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSDockIcon_test
+//
+//  Created by Gregory Casamento on 5/29/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSDockIcon_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSDockIcon_test
+//
+//  Created by Gregory Casamento on 5/29/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
